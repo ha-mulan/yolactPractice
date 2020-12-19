caccbff07eb599e8f4e8dd72dc4ff31739d3a54f
ha-mulan <ham5312@naver.com>
2020-12-19 11:50:05 +0000

    first commint

--- data/scripts/COCO.sh
+++ data/scripts/COCO.sh
@@ -30,8 +30,8 @@ fi
 
 # Download the image data.
 cd ./images
-echo "Downloading MSCOCO train images ..."
-curl -LO http://images.cocodataset.org/zips/train2017.zip
+#echo "Downloading MSCOCO train images ..."
+#curl -LO http://images.cocodataset.org/zips/train2017.zip
 echo "Downloading MSCOCO val images ..."
 curl -LO http://images.cocodataset.org/zips/val2017.zip
 
@@ -44,24 +44,24 @@ fi
 # Download the annotation data.
 cd ./annotations
 echo "Downloading MSCOCO train/val annotations ..."
-curl -LO http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+#curl -LO http://images.cocodataset.org/annotations/annotations_trainval2014.zip
 curl -LO http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 echo "Finished downloading. Now extracting ..."
 
 # Unzip data
-echo "Extracting train images ..."
-unzip -qqjd ../images ../images/train2017.zip
+#echo "Extracting train images ..."
+#unzip -qqjd ../images ../images/train2017.zip
 echo "Extracting val images ..."
 unzip -qqjd ../images ../images/val2017.zip
 echo "Extracting annotations ..."
-unzip -qqd .. ./annotations_trainval2014.zip
+#unzip -qqd .. ./annotations_trainval2014.zip
 unzip -qqd .. ./annotations_trainval2017.zip
 
-echo "Removing zip files ..."
-rm ../images/train2017.zip
-rm ../images/val2017.zip
-rm ./annotations_trainval2014.zip
-rm ./annotations_trainval2017.zip
+#echo "Removing zip files ..."
+#rm ../images/train2017.zip
+#rm ../images/val2017.zip
+#rm ./annotations_trainval2014.zip
+#rm ./annotations_trainval2017.zip
 
 
 end=`date +%s`
